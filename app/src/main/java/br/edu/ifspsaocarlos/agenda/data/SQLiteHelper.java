a9478b963675c4b4056a6b28c85d4d21c531d94c
Alex <alexpfragoso@gmail.com>
2017-12-11 19:35:05 -0200

    Initial commit
    - Todos os arquivos de classes criados inicialmente vazio.
    - Classe Contato foi implementada.
    - Grade foi modificado de acordo com a proposta do projeto.

---
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
@@ -0,0 +1,8 @@
+package br.edu.ifspsaocarlos.agenda.data;
+
+/**
+ * Created by Alex Fragoso on 11/12/2017.
+ */
+
+public class SQLiteHelper {
+}
---
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/view/DetalheActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ifspsaocarlos.agenda.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ifspsaocarlos.agenda.R;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}

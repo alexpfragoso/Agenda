8a941b13b04e5455abc4887e662d167b82c8d80f
Alex <alexpfragoso@gmail.com>
2017-12-12 18:16:37 -0200

    - implementação da classe SQLiteHelper

--- app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
@@ -1,8 +1,46 @@
 package br.edu.ifspsaocarlos.agenda.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
 /**
  * Created by Alex Fragoso on 11/12/2017.
  */
 
-public class SQLiteHelper {
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    //VARIÁVEIS
+    public static final String DATABASE_NAME = "agenda.db";
+    public static final String DATABASE_TABLE = "contatos";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "nome";
+    public static final String KEY_FONE = "fone";
+    public static final String KEY_EMAIL = "email";
+
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_CREATE = "CREATE TABLE" + DATABASE_TABLE  + "(" +
+            KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            KEY_NAME + "TEXT NOT NULL, " +
+            KEY_FONE + "TEXT, " +
+            KEY_EMAIL + "TEXT);";
+
+
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 }
